9c12304b9343610807b7387b790922b64f819099
Ryan Davis <ryan.davis@infarmbureau.com>
2019-04-30 12:58:54 -0400

    Fixed rest engine configuration, Switched to profile activated Dockerfile builds, Added base property file configurations

--- src/main/resources/archetype-resources/src/main/groovy/com/infarmbureau/__rootArtifactId__/bpm/ProcessEngineConfiguration.groovy
+++ src/main/resources/archetype-resources/src/main/groovy/com/infarmbureau/__rootArtifactId__/bpm/ProcessEngineConfiguration.groovy
@@ -1,17 +1,23 @@
 package com.infarmbureau.${rootArtifactId}.bpm
 
-import com.infarmbureau.${rootArtifactId}.bpm.util.AppProperties
+import com.infarmbureau.demo.bpm.util.AppProperties
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.datasource.SimpleDriverDataSource
+
+import javax.sql.DataSource
 
 @Configuration
+@Profile("prod")
 class ProcessEngineConfiguration {
 
     @Autowired
     AppProperties properties
 
+////    Uncomment to activate external database. (Be sure to configure .properties correctly)
 //    @Bean
-//    // Uncomment this bean to configure a persistent database solution.
 //    DataSource dataSource() {
 //        SimpleDriverDataSource dataSource = new SimpleDriverDataSource()
 //        dataSource.setDriverClass(properties.jdbc.driver)
