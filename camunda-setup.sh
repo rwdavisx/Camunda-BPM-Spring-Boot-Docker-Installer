054354e78102113412221ac25c71e9eac9821e82
Ryan Davis <ryan.davis@infarmbureau.com>
2019-12-10 13:28:26 -0500

    Modified to use camunda community

--- camunda-setup.sh
+++ camunda-setup.sh
@@ -5,15 +5,15 @@ if [ -z "${ARTIFACT_ID}" ]; then
   exit 1
 fi
 
-if [ -z "${EE_USERNAME}" ]; then
-  echo "Provide the Camunda EE username with -e EE_USERNAME=validUsername"
-  exit 1
-fi
-
-if [ -z "${ARTIFACT_ID}" ]; then
-  echo "Provide the Camunda EE password with -e EE_PASSWORD=validPassword"
-  exit 1
-fi
+#if [ -z "${EE_USERNAME}" ]; then
+#  echo "Provide the Camunda EE username with -e EE_USERNAME=validUsername"
+#  exit 1
+#fi
+#
+#if [ -z "${ARTIFACT_ID}" ]; then
+#  echo "Provide the Camunda EE password with -e EE_PASSWORD=validPassword"
+#  exit 1
+#fi
 
 mkdir -p /app/src/"$ARTIFACT_ID"
 
@@ -25,5 +25,6 @@ mvn archetype:generate \
   -DinteractiveMode=false
 
 cd "$ARTIFACT_ID"
-mvn clean install -s settings.xml -Dee_username=$EE_USERNAME -Dee_password=$EE_PASSWORD && \
+mvn clean install \
+#  -gs settings.xml -Dee_username=$EE_USERNAME -Dee_password=$EE_PASSWORD
 cp -R * /app/src/"$ARTIFACT_ID"
