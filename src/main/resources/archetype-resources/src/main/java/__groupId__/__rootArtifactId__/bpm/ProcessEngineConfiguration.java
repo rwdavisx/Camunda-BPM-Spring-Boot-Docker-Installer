a206cf292b0bb58e86833606f4bad8a149710bfe
Ryan Davis <ryan.davis@infarmbureau.com>
2020-03-12 15:41:25 -0400

    Cleaned up unneeded code

--- src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/ProcessEngineConfiguration.java
+++ src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/ProcessEngineConfiguration.java
@@ -2,28 +2,12 @@ package ${groupId}.${rootArtifactId}.bpm;
 
 import ${groupId}.${rootArtifactId}.bpm.util.AppProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
-@Profile("dev")
+@Profile("default")
 public class ProcessEngineConfiguration {
-
     @Autowired
     AppProperties properties;
-
-////    Uncomment to activate an external database. (Be sure to configure application-{env}.properties correctly)
-//    @Bean
-//    DataSource dataSource() {
-//        SimpleDriverDataSource dataSource = new SimpleDriverDataSource()
-//        dataSource.setDriverClass(properties.jdbc.driver)
-//        dataSource.setUrl(properties.jdbc.url)
-//        dataSource.setUsername(properties.jdbc.username)
-//        dataSource.setPassword(properties.jdbc.password)
-//        return dataSource
-//    }
 }
--- src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/util/AppProperties.java
+++ src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/util/AppProperties.java
@@ -1,27 +1,10 @@
 package ${groupId}.${rootArtifactId}.bpm.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("ifbi")
+//@ConfigurationProperties("")
 public class AppProperties {
 
-    @Autowired
-    Jdbc jdbc;
-
-    @Component
-    @ConfigurationProperties("ifbi.jdbc")
-    static class Jdbc {
-
-        String url;
-
-        String username;
-
-        String password;
-
-        Class driver;
-
-    }
 }
