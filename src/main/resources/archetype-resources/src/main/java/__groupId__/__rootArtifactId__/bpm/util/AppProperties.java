1cacdc3a44da8bc20370618b9aab1a5e55c68c8b
Ryan Davis <ryan.davis@infarmbureau.com>
2020-06-02 12:03:10 -0500

    Fixes

--- src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/util/AppProperties.java
+++ src/main/resources/archetype-resources/src/main/java/__groupId__/__rootArtifactId__/bpm/util/AppProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-//@ConfigurationProperties("")
 public class AppProperties {
 
 }
